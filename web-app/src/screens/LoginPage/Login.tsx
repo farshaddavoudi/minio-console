8d488ced69ad86e9259780f4d9218273579fb2d1
Farshad Davoudi <f.davoudi.r@outlook.com>
2025-05-11 14:14:38 +0330

    Change login page header

--- web-app/src/screens/LoginPage/Login.tsx
+++ web-app/src/screens/LoginPage/Login.tsx
@@ -194,7 +194,7 @@ const Login = () => {
         }
         promoHeader={
           <span style={{ fontSize: 28 }}>
-            ATA 332 High-Performance Object Store
+            فضای ابری آتا
           </span>
         }
         promoInfo={
